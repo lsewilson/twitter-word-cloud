8b73cb0a71844ac4153efe803c9664bc10f78d20
lsewilson <laurawilson@hotmail.co.uk>
2016-11-16 07:04:55 +0000

    refactored test

--- spec/features/word_cloud_feature_spec.rb
+++ spec/features/word_cloud_feature_spec.rb
@@ -6,7 +6,6 @@ feature 'Word Cloud' do
     expect(page).to have_selector('#cloud:empty')
     fill_in 'username', with: 'twitter'
     click_button 'Submit'
-    expect(page.status_code).to eq 200
     expect(page).to have_current_path '/'
   end
 end
